bd7f2767632d779f41c9f545310a093f2a6e629a
statsconchris <68184749+statsconchris@users.noreply.github.com>
2021-03-30 17:25:45 -0500

    Update rouletteStochastic.m

--- rouletteStochastic.m
+++ rouletteStochastic.m
@@ -1,5 +1,5 @@
 function rouletteStochastic(input) %e.g., fixed: input = 0. random: input = 1
-%game variables
+%variables del juego
 laps     = 1000000; 
 gameFR   = randi([0,36],1,laps);
 gameUS   = randi([0,37],1,laps); % 37 == 00 
@@ -9,7 +9,7 @@ winFR    = 0;
 winUS    = 0;
 
         function number = FixedNumberBet()
-            %game FR
+            %juego FR
             number = randi([0,36]);
 
             for i = 1:laps
@@ -21,7 +21,7 @@ winUS    = 0;
                 end
             end
             
-            %game US
+            %juego US
             for i = 1:laps
                 if   gameUS(i) == number
                           pointsUS(i) = pointsUS(i) + 35;
@@ -35,7 +35,7 @@ winUS    = 0;
 
     
         function RandomNumberBet() 
-            %game FR
+            %juego FR
             for i = 1:laps
                 if   gameFR(i) == randi([0,36])
                           pointsFR(i) = pointsFR(i) + 35;
@@ -45,7 +45,7 @@ winUS    = 0;
                 end
             end
             
-            %game US
+            %juego US
             for i = 1:laps
                 if   gameUS(i) == randi([0,36])
                           pointsUS(i) = pointsUS(i) + 35;
@@ -60,18 +60,18 @@ winUS    = 0;
 
     if input == 0
         input = FixedNumberBet();
-        title   = ['Your bet was #', num2str(input), ' in ' num2str(laps) ' games.'];
-        x       = ['You won ', num2str(winFR * 100 / laps),'% games with ', num2str(sum(pointsFR)),' (', num2str(((laps + sum(pointsFR))/(2*laps)) * 100), '%) points in FR roulette.'];
-        y       = ['You won ', num2str(winUS * 100 / laps),'% games with ', num2str(sum(pointsUS)),' (', num2str(((laps + sum(pointsUS))/(2*laps)) * 100), '%) points in US roulette.'];
+        title   = ['Su apuesta fue #', num2str(input), ' en ' num2str(laps) ' juegos.'];
+        x       = ['Ud. gano ', num2str(winFR * 100 / laps),'% juegos con ', num2str(sum(pointsFR)),' (', num2str(((laps + sum(pointsFR))/(2*laps)) * 100), '%) puntos en la ruleta FR.'];
+        y       = ['Ud. gano ', num2str(winUS * 100 / laps),'% juegos con ', num2str(sum(pointsUS)),' (', num2str(((laps + sum(pointsUS))/(2*laps)) * 100), '%) puntos en la ruleta US.'];
 
         disp(title)
         disp(x)
         disp(y)
     else
         RandomNumberBet();
-        title   = ['Your bet was Random in ', num2str(laps), ' games.'];
-        x       = ['You won ', num2str(winFR * 100 / laps),'% games with ', num2str(sum(pointsFR)),' (', num2str(((laps + sum(pointsFR))/(2*laps)) * 100), '%) points in FR roulette.'];
-        y       = ['You won ', num2str(winUS * 100 / laps),'% games with ', num2str(sum(pointsUS)),' (', num2str(((laps + sum(pointsUS))/(2*laps)) * 100), '%) points in US roulette.'];
+        title   = ['Su apuesta fue Random en ', num2str(laps), ' juegos.'];
+        x       = ['Ud. gano ', num2str(winFR * 100 / laps),'% juegos con ', num2str(sum(pointsFR)),' (', num2str(((laps + sum(pointsFR))/(2*laps)) * 100), '%) puntos en la ruleta FR.'];
+        y       = ['Ud. gano ', num2str(winUS * 100 / laps),'% juegos con ', num2str(sum(pointsUS)),' (', num2str(((laps + sum(pointsUS))/(2*laps)) * 100), '%) puntos en la ruleta US.'];
 
         disp(title)
         disp(x)
